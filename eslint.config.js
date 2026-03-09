19286e51bb18e05154540b748762846ac40b1499
Ludmila Andrushenko <ludmila.andrushenko@4k.com.ua>
2026-03-09 18:58:06 +0200

    chore: remove unused imports

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,5 @@
 import js from '@eslint/js';
 import eslintConfigPrettier from 'eslint-config-prettier';
-import prettier from 'eslint-plugin-prettier';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import { defineConfig, globalIgnores } from 'eslint/config';
