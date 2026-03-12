2801f16c9616f96ff8169330d8325a0a2bf031a9
Oleksandra (SASHA) Honchar <88204002+SashaHon@users.noreply.github.com>
2026-03-12 22:28:40 +0100

    extend primary styles and fonts

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 
 import App from '@/App.tsx';
 import '@/index.css';
+import '@/styles/fonts.scss';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
