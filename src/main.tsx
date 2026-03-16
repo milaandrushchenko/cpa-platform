67e83b72de609c3b3a29601c6b270e5eb3e7f341
Oleksandra (SASHA) Honchar <88204002+SashaHon@users.noreply.github.com>
2026-03-16 20:42:57 +0100

    make color tokens more semantic and add black bg as default bg

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
+import '@styles/fonts.scss';
+import '@styles/index.scss';
+
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from '@/App.tsx';
-import '@/styles/fonts.scss';
-import '@/styles/index.scss';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
