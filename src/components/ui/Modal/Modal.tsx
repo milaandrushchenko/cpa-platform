001d71ae29c0cee46501e02385e2a81887283692
Ludmila Andrushenko <ludmila.andrushenko@4k.com.ua>
2026-03-20 17:20:51 +0200

    feat: add Modal component

---
+++ src/components/ui/Modal/Modal.tsx
@@ -0,0 +1,48 @@
+import CloseIcon from '@assets/icons/close.svg?react';
+import clsx from 'clsx';
+
+import { type MouseEvent, type PropsWithChildren, useEffect } from 'react';
+
+import { CloseButton } from '../icons/CloseButton/CloseButton';
+import styles from './Modal.module.scss';
+
+type ModalProps = PropsWithChildren<{
+  isOpen: boolean;
+  onClose: () => void;
+  className?: string;
+}>;
+
+export const Modal = ({ isOpen, onClose, className, children }: ModalProps) => {
+  useEffect(() => {
+    if (!isOpen) return;
+
+    const handleEsc = (event: KeyboardEvent) => {
+      if (event.key === 'Escape') {
+        onClose();
+      }
+    };
+
+    document.addEventListener('keydown', handleEsc);
+
+    return () => {
+      document.removeEventListener('keydown', handleEsc);
+    };
+  }, [isOpen, onClose]);
+
+  const handleOverlayClick = (event: MouseEvent<HTMLDivElement>) => {
+    if (event.target === event.currentTarget) {
+      onClose();
+    }
+  };
+
+  if (!isOpen) return null;
+
+  return (
+    <div className={styles.overlay} onClick={handleOverlayClick}>
+      <div className={clsx(styles.modal, className)}>
+        <CloseButton onClick={onClose} className={styles.closeButton} />
+        {children}
+      </div>
+    </div>
+  );
+};
