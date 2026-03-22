b5a0d29fe35fab9f171e5a976dfbb5833e9dcb51
Ludmila Andrushenko <ludmila.andrushenko@4k.com.ua>
2026-03-23 00:14:14 +0200

    feat: create ApplicationForm

---
+++ src/components/ApplicationForm/ApplicationForm.tsx
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+
+import { SelectField } from '@/components/ui/SelectField/SelectField';
+import { TextField } from '@/components/ui/TextField/TextField';
+
+import styles from './ApplicationForm.module.scss';
+
+export const ApplicationForm = () => {
+  const [name, setName] = useState('');
+  const [contactMethod, setContactMethod] = useState('');
+  const [contact, setContact] = useState('');
+
+  return (
+    <form className={styles.form}>
+      <TextField
+        label="Your Name"
+        name="name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+
+      <div className={styles.row}>
+        <SelectField
+          className={styles.contactMethod}
+          label="Contact Method"
+          name="contactMethod"
+          value={contactMethod}
+          onChange={setContactMethod}
+          isRequiredMark
+          options={[
+            { label: 'Telegram', value: 'telegram' },
+            { label: 'Email', value: 'email' },
+            { label: 'Phone', value: 'phone' },
+          ]}
+        />
+
+        <TextField
+          className={styles.contactValue}
+          label="Your Contact"
+          name="contact"
+          value={contact}
+          onChange={(e) => setContact(e.target.value)}
+          isRequiredMark
+          required
+        />
+      </div>
+    </form>
+  );
+};
