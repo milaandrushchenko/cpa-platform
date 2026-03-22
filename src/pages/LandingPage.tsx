29fb9a414fa2e82dd27159b16c6059365ab47efa
Ludmila Andrushenko <ludmila.andrushenko@4k.com.ua>
2026-03-23 00:14:38 +0200

    refactor: styles for fields

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -2,16 +2,30 @@ import { Button } from '@components/ui/Button';
 import { useScrollToHash } from '@hooks/useScrollToHash';
 import { PageLayout } from '@layouts/PageLayout';
 
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
+
+import { ApplicationForm } from '@/components/ApplicationForm/ApplicationForm';
+import { Modal } from '@/components/ui/Modal';
 
 export default function LandingPage() {
   const { t } = useTranslation();
   useScrollToHash();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+  const closeModal = () => setIsModalOpen(false);
 
   return (
     <main>
       <PageLayout id="hero" fullHeight>
         {t('hero.sectionName')}
+        <Button onClick={openModal}>Open modal</Button>
+        <Modal isOpen={isModalOpen} onClose={closeModal}>
+          <ApplicationForm />
+        </Modal>
       </PageLayout>
       <PageLayout id="tasks" fullHeight>
         {t('tasks.sectionName')}
