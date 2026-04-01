9ef5936257cac674b28fad447b239390e0c179e1
Ludmila Andrushenko <ludmila.andrushenko@4k.com.ua>
2026-04-01 17:58:20 +0300

    feat: display all api data + add styles

--- src/sections/benefitsSection/BenefitsSection.tsx
+++ src/sections/benefitsSection/BenefitsSection.tsx
@@ -1,4 +1,5 @@
 import { PageLayout } from '@layouts/PageLayout';
+import clsx from 'clsx';
 
 import { useTranslation } from 'react-i18next';
 
@@ -11,24 +12,42 @@ type BenefitsSectionProps = {
   data: BenefitsResponse;
 };
 
+const ACCENT_TEXT = 'guaranteed';
+
 export default function BenefitsSection({ data }: BenefitsSectionProps) {
   const { t } = useTranslation();
 
-  console.log('benefits', data);
+  const hasAccent = data.title.includes(ACCENT_TEXT);
+  const parts = hasAccent ? data.title.split(ACCENT_TEXT) : [];
 
   return (
-    <PageLayout id="benefits" fullHeight className={styles.bg}>
+    <PageLayout
+      id="benefits"
+      fullHeight
+      className={clsx(styles.bg, styles.section)}
+    >
       <h2 className={styles.sectionLabel}>{t('benefits.sectionName')}</h2>
       <SectionGrid desktop={{ columns: 2, gap: '30px', ratios: [1.3, 1] }}>
-        <div>
-          <h3>{data.title}</h3>
-          <p>{data.description}</p>
+        <div className={styles.firstColumn}>
+          <h3 className={styles.title}>
+            {hasAccent ? (
+              <>
+                {parts[0]}
+                <span className={styles.accent}>{ACCENT_TEXT}</span>
+                {parts[1]}
+              </>
+            ) : (
+              data.title
+            )}
+          </h3>
+          <p className={styles.description}>{data.description}</p>
         </div>
-        <div>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque aperiam
-          eos accusantium, alias repellendus, odio fugit vero magni
-          reprehenderit in quod ex voluptate, laborum quis. Consequuntur
-          praesentium distinctio voluptatum doloribus.
+        <div className={styles.benefitsList}>
+          {data.benefits.map((benefit) => (
+            <div className={styles.benefitsItem} key={benefit}>
+              {benefit}
+            </div>
+          ))}
         </div>
       </SectionGrid>
     </PageLayout>
