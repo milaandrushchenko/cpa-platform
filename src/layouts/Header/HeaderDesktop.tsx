f61f5239c68461b661a65d57e47f71518318b48c
Ludmila Andrushenko <ludmila.andrushenko@4k.com.ua>
2026-06-03 15:56:23 +0300

    fix: fix header links

--- src/layouts/Header/HeaderDesktop.tsx
+++ src/layouts/Header/HeaderDesktop.tsx
@@ -1,24 +1,25 @@
-import { LogoIcon } from '@components/ui/icons/LogoIcon';
-
 import { BurgerMenu } from '@/components/BurgerMenu';
 import { LanguageSwitcher } from '@/components/LanguageSwitcher';
 import { NavLinks } from '@/components/NavLinks';
+import { LogoIcon } from '@/components/ui/icons/LogoIcon';
+import { PageLayout } from '@/layouts/PageLayout';
 
-import { PageContainer } from '../PageContainer';
 import style from './Header.module.scss';
 
 const HeaderDesktop = () => {
   return (
-    <PageContainer>
-      <div className={style.headerContainer}>
-        <LogoIcon />
-        <div className={style.navActions}>
-          <NavLinks variant="row" />
-          <LanguageSwitcher />
+    <header className={style.headerWrapper}>
+      <PageLayout as="div">
+        <div className={style.headerContainer}>
+          <LogoIcon />
+          <div className={style.navActions}>
+            <NavLinks variant="row" />
+            <LanguageSwitcher />
+          </div>
+          <BurgerMenu />
         </div>
-        <BurgerMenu />
-      </div>
-    </PageContainer>
+      </PageLayout>
+    </header>
   );
 };
 
